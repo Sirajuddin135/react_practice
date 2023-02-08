e8b9b89eb2f7cea49a4266c75412e37622bc4a7f
Shaik Sirajuddin <101395494+Sirajuddin135@users.noreply.github.com>
2023-02-08 22:49:30 +0530

    Text.js updated

--- src/Text.js
+++ src/Text.js
@@ -19,3 +19,40 @@ export const Text = () => {
         </div>
     )
 }
+
+import './App.css';
+import { useState } from 'react';
+import { Text } from './Text';
+
+function App() {
+
+  // CSS, Ternary Operator
+  // let isTextColorful = true;
+  // let isButtonPresent = false;
+
+  // return (
+  //   <div className='App'>
+  //     <h1 style={{ color: isTextColorful ? "green" : "red" }}>This text is colorful</h1>
+  //     {isButtonPresent && <button>This is a button</button>}
+  //   </div >
+  // );
+
+  const [showText, setShowText] = useState(false);
+
+  return (
+    <div className='App'>
+      <button
+        onClick={() =>
+          setShowText(!showText)
+        }
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
+    </div >
+  )
+}
+
+export default App;
+
